32e99975d63d3700d2dba56aed175cdc65958097
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-08-05 14:18:41 -0300

    Names corretos

--- public/js/candidatos.js
+++ public/js/candidatos.js
@@ -1,29 +1,68 @@
+let botaoNao = document.querySelector('#btn_nao');
+let botaoSim = document.querySelector('#btn_check');
+let telaParentes = document.querySelector('.tela_parentes');
 
-
-  let botaoNao = document.querySelector('#btn_nao');
-  let botaoSim = document.querySelector('#btn_check');
-
-  function alternarBotoes() {
-    if (!botaoSim.disabled) {
-      botaoSim.disabled = true;
-      botaoSim.style.backgroundColor = "blue";
-      botaoSim.style.color = "white";
-      botaoNao.style.backgroundColor = "initial";
-      botaoNao.style.color = "initial";
-    } else {
-      botaoSim.disabled = false;
-      botaoSim.style.backgroundColor = "initial";
-      botaoSim.style.color = "initial";
-      botaoNao.style.backgroundColor = "blue";
-      botaoNao.style.color = "white";
-    }
+function alternarBotoes() {
+  if (botaoSim.disabled) {
+    botaoSim.disabled = false;
+    botaoSim.style.backgroundColor = "initial";
+    botaoSim.style.color = "initial";
+    botaoNao.style.backgroundColor = "blue";
+    botaoNao.style.color = "white";
+    telaParentes.style.display = "block"; // Exibe a div de parentes
+  } else {
+    botaoSim.disabled = true;
+    botaoSim.style.backgroundColor = "blue";
+    botaoSim.style.color = "white";
+    botaoNao.style.backgroundColor = "initial";
+    botaoNao.style.color = "initial";
+    telaParentes.style.display = "none"; // Esconde a div de parentes
   }
+}
 
-  botaoSim.addEventListener('click', alternarBotoes);
-  botaoNao.addEventListener('click', alternarBotoes);
+botaoSim.addEventListener('click', alternarBotoes);
+botaoNao.addEventListener('click', alternarBotoes);
 
   // Função para inserir dependentes:
-  let tela_dep = document.querySelector(".tela_dependentes");
+  document.getElementById("btn_dependentes").onclick = function () {
+    let texto = "";
+    texto += `
+    <label> Digite as informações do dependente: </label>
+    <input name="dependente_nome" type="text" placeholder="Insira o nome:">
+    <input name="dependente_cpf" type="number" placeholder="Insira o cpf:">
+    <select name="dependente_sexo" required="required" id="sexo">
+        <option>Informe o sexo</option>
+        <option value="masculino">Masculino</option>
+        <option value="feminino">Feminino</option>
+    </select>
+    <input type="date" name="dependente_data_nascimento">
+    <input type="text" name="dependente_grau_parentesco" placeholder="Informe o grau de parentesco:">
+    `;
+    telaDependentes.innerHTML = texto;
+  };
+
+  // FUnção para inserir parentes
+  document.getElementById("btn_check").onclick = function () {
+    let string = "";
+    if (botaoSim.disabled) {
+      string += `
+        <label>Insira o nome:</label>
+        <input type="text" name="parente_nome" placeholder="Insira o nome:">
+        <input type="text" name="parente_cargo" placeholder="Insira o cargo:">
+        <input type="text" name="parente_cidade" placeholder="Insira a cidade:">
+      `;
+      telaParentes.innerHTML = string;
+      telaParentes.style.display = "block";
+    } else {
+      telaParentes.innerHTML = "";
+      telaParentes.style.display = "none"; // Esconde a div de parentes
+    }
+  };
+
+
+
+  // Função para inserir dependentes:
+  let telaDependentes = document.querySelector(".tela_dependentes");
   document.getElementById("btn_dependentes").onclick = function () {
     let texto = "";
     texto += `
@@ -38,11 +77,10 @@
     <input type="date" name="data">
     <input type="text" name="grau" placeholder="Informe o grau de parentesco:">
     `;
-    tela_dep.innerHTML = texto;
+    telaDependentes.innerHTML = texto;
   };
 
   // FUnção para inserir parentes
-  let tela_parentes = document.querySelector(".tela_parentes");
   document.getElementById("btn_check").onclick = function () {
     let string = "";
     if (botaoSim.disabled) {
@@ -52,9 +90,11 @@
         <input type="text" name="" placeholder="Insira o cargo:">
         <input type="text" placeholder="Insira a cidade:">
       `;
-      tela_parentes.innerHTML = string;
+      telaParentes.innerHTML = string;
+      telaParentes.style.display = "block";
     } else {
-      tela_parentes.innerHTML = "";
+      telaParentes.innerHTML = "";
+      telaParentes.style.display = "none";
     }
   };
 
